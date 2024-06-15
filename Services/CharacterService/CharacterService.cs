38af14fc698cbb5faf236b3a5f2a8a2b16dd7427
Alex Kovalyov <alex.kovalyov@t-a9.com>
2024-06-15 19:57:42 +0800

    add first controller

---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,34 @@
+﻿using dotnet_rpg.Models;
+
+namespace dotnet_rpg.Services.CharacterService;
+
+public class CharacterService:ICharacterService
+
+
+{
+    private static List<Character> knights = new List<Character>
+    {
+        new Character(1, "Alex", 25, 25, 25, 25, RpgClass.Cleric),
+        new Character(2, "Alex", 25, 25, 25, 25, RpgClass.Cleric),
+
+
+    };
+    
+    public  async Task<List<Character>> GetAllCharacters()
+    {
+        return knights;
+    }
+
+    public async Task<Character> GetCharacterById(int id)
+    {
+        var character = knights.FirstOrDefault(c => c.Id == id);
+        if (character != null) return character;
+        throw new Exception("Character doesnt exists");
+    }
+
+    public async Task<List<Character>> AddNewCharacter(Character character)
+    {
+        knights.Add(character);
+        return knights;
+    }
+}
